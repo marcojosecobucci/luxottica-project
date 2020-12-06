1c55e55908cb2ac8f8be6318de6ad594d0078d9c
marcojosecobucci <marco.cobucci@outlook.com>
2020-12-07 00:10:40 +0100

    animazioni saving e footer mobile

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-
+// savings menu scroll desktop
 $('#savings').click(function() {
     $('.midbar__down').slideToggle();
 });
-
+// ricerca al click di btn desktop
 $('.btn__search').click(function() { 
     $('#input__search').slideToggle();
 })
+// menu burgher mobile
 $( ".cross" ).hide();
 $( ".menu__burgher__list" ).hide();
 $( ".btn__hamburgher" ).click(function() {
@@ -22,6 +23,18 @@ $( ".cross" ).hide();
 $( ".btn__hamburgher" ).show();
 });
 });
+// slide toggle footer ul
+$('#btn__footer').click(function() {
+    $('.footerbar__down').slideToggle();
+});
+$('#btn__footer1').click(function() {
+    $('.footerbar__down1').slideToggle();
+});
+
+$('#btn__footer2').click(function() {
+    $('.footerbar__down2').slideToggle();
+});
+
 
 });
 
